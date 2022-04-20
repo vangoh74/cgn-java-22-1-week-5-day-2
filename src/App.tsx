a6871325f31cd3ef5c5c1e26bb358a4425b8c855
Vandicleia Goncalves <vangoh05@protonmail.com>
2022-04-20 17:07:44 +0200

    router_hooks_task

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Card } from './model/Card';
+import CharacterDetailsPage from './pages/CharacterDetailsPage';
 import GalleryPage from './pages/GalleryPage';
 import Header from './pages/Header';
 import ImpressumPage from './pages/ImpressumPage';
 
 export default function App() {
+
     return (
         <BrowserRouter>
             <Header />
             <Routes>
                 <Route path={"/"} element={<ImpressumPage />} />
                 <Route path={"/gallery"} element={<GalleryPage />} />
+                <Route path={'/character/:id'} element={<CharacterDetailsPage />} />
             </Routes>
         </BrowserRouter>
     );
--- src/pages/CharacterDetailsPage.tsx
+++ src/pages/CharacterDetailsPage.tsx
@@ -1,10 +1,42 @@
+import {useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import PostCard from "../components/PostCard";
+import { Card } from "../model/Card";
+import "./CharacterDetailsPage.css";
 
 export default function CharacterDetailsPage() {
     const params = useParams();
     const id = params.id;
+    const idAsNbr = parseInt(id as string);
+    const [character, setCharacter] = useState<Card>();
 
+    const fetchCharacter = () => {
+        return fetch(`https://rickandmortyapi.com/api/character/${id}`)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error("Network error")
+            })
+            .catch(console.error);
+    }
+
+
+    useEffect(() => {
+        fetchCharacter()
+            .then(char => setCharacter(char))
+    }, [])
+
+
+
+
+    console.log(character);
+    console.log(character);
     return (
-        <>Character {id}</>
+
+        <div>
+            "Das ist nur ein Test!"
+            {character && <PostCard card = {character}/>}
+        </div>
     )
 }
